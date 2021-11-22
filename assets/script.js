305e3d611b9e2f3fd26656a59acb46509d2f106b
rclank <53981655+rclank@users.noreply.github.com>
2021-11-22 02:38:06 -0500

    all weather logic complete?

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,7 @@
 // DOM elements
 const searchFormEl =  document.querySelector('#search-form');
 const cityInputEl = document.querySelector('#search');
+const weatherDashboardEl = document.querySelector('#dashboard');
 
 // variables
 const apiKey = "b91170236f07db86ffa15047cc1bb3bb";
@@ -90,21 +91,14 @@ const checkWeather = function(geoData) {
         })
 }
 
-// DOM elements
-const weatherDashboardEl = document.querySelector('#dashboard');
-// const weatherTodayCardEl = document.querySelector('#weather-today');
-// const forecastContainerEl = document.querySelector('#forecast');
-
-
 const buildDashboard = function(cityWeatherData) {
+    clearWeatherElements();
 
     console.log('city weather', cityWeatherData);
     buildWeatherToday(cityWeatherData);
 
-    // buildForecast(cityWeatherData);
+    buildForecast(cityWeatherData);
 }
-
-const dateFormat = 'today.getDate()/today.getMonth()/today.getFullYear()';
 
 const buildWeatherToday = function(cityWeatherData) {
     const weatherToday = cityWeatherData.current;
@@ -116,11 +110,118 @@ const buildWeatherToday = function(cityWeatherData) {
     const weatherTodayCardEl = document.createElement('div');
     weatherTodayCardEl.setAttribute('id', 'weather-today');
 
+    // create header
     const headerEl = document.createElement('h2');
+    headerEl.textContent = `${city} (${dateBuilder(today)})`;
+    // create img element for weather icon
+    const iconEl = document.createElement('img');
+    iconEl.setAttribute('src', getWeatherIcon(weatherToday));
+    // append icon to header
+    headerEl.appendChild(iconEl);
+    // append header to weather today
+    weatherTodayCardEl.appendChild(headerEl);
 
-    const todayz = `${city} (${dateFormat})`;
-    console.log(todayz);
-    headerEl.textContent = `${city} (${dateFormat})`;
+    displayWeatherData(weatherTodayCardEl, weatherToday);
+
+    // append to weather dashboard
+    weatherDashboardEl.appendChild(weatherTodayCardEl);
+}
+
+const buildForecast = function(cityWeatherData) {
+    const forecastLength = 5;
+
+    // create header
+    const forecastHeaderEl = document.createElement('h3');
+    forecastHeaderEl.textContent = '5-Day Forecast:';
+
+    // create forecast container
+    const forecastEl = document.createElement('div');
+    forecastEl.setAttribute('id', 'forecast');
+    forecastEl.classList.add('d-flex', 'flex-wrap', 'justify-content-between');
+
+    // loop for forecast cards
+    buildDailyForecast(forecastEl, cityWeatherData, forecastLength);
+
+    // append header to dashboard container
+    weatherDashboardEl.appendChild(forecastHeaderEl);
+    // append forecast container
+    weatherDashboardEl.appendChild(forecastEl);
+}
+
+// to be called at the start of new weather data being processed
+const clearWeatherElements = function() {
+    weatherDashboardEl.innerHTML = "";
+}
+
+const dateBuilder = function(date, daysPlus = 0) {
+    // not sure why setting targetDate has to be this way, but setting to just '= date' updates the global today for some reason?
+    const targetDate = new Date(date);
+    targetDate.setDate(targetDate.getDate() + daysPlus);
+
+    const stringDate = targetDate.getMonth() + "/" + targetDate.getDate() + "/" + targetDate.getFullYear();
+    return stringDate;
+}
+
+const getWeatherIcon = function(cityWeatherData) {
+    const weatherIcon = cityWeatherData.weather[0].icon;
+    return "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+}
+
+const buildDailyForecast = function(parentEl = forecastEl, cityWeatherData, forecastLength = 5) {
+    const dailyWeatherData = cityWeatherData.daily;
+
+    for (let i = 0; i < forecastLength; i++) {
+        const currentForecastCardEl = buildForecastCard(dailyWeatherData[i], i);
+        parentEl.appendChild(currentForecastCardEl);
+    }
+}
+
+const displayWeatherData = function(displayEl, cityWeatherData) {
+    // create temp p
+    const tempEl = document.createElement('p');
+    let tempData = cityWeatherData.temp;
+    if (typeof tempData === 'object') {
+        tempData = cityWeatherData.temp.day;
+    }
+    tempEl.textContent = 'Temp: ' + tempData + '°F';
+
+    // create wind p
+    const windEl = document.createElement('p');
+    windEl.textContent = 'Wind: ' + cityWeatherData.wind_speed + ' MPH';
+
+    // create humidity p
+    const humidityEl = document.createElement('p');
+    humidityEl.textContent = 'Humidity: ' + cityWeatherData.humidity + '%';
+
+    // append above elements
+    displayEl.append(tempEl, windEl, humidityEl);
+
+    // conditional for today vs forecast
+    if (displayEl.getAttribute('id') === 'weather-today') {
+        const uviEl = document.createElement('p');
+        uviEl.textContent = 'UV Index: ' + cityWeatherData.uvi;
+        displayEl.appendChild(uviEl);
+    } else {
+        const iconEl = document.createElement('img');
+        iconEl.setAttribute('src', getWeatherIcon(cityWeatherData));
+        // append as first child
+        displayEl.insertBefore(iconEl, tempEl);
+    }
+}
+
+const buildForecastCard = function(cityWeatherData, daysPlus = 0) {
+    const forecastCardEl = document.createElement('div');
+    forecastCardEl.classList.add('forecast-card');
+
+    // create and append card date headers
+    const headerDateEl = document.createElement('h4');
+    headerDateEl.textContent = dateBuilder(today, daysPlus);
+    forecastCardEl.appendChild(headerDateEl);
+
+    // get weather data
+    displayWeatherData(forecastCardEl, cityWeatherData);
+
+    return forecastCardEl;
 }
 
 // call getCityWeather on most recent city, or feed city if none
